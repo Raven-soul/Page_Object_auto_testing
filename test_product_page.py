33478c099ca16c6f2e1f84763eec238b18f70603
Raven-soul <mr.kolin.199@mail.ru>
2023-01-20 02:13:39 +0300

    group test
    7 answer

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from .pages.product_page import ProductPage
 import time 
+import pytest
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('test_site', ["promo=offer0", "promo=offer1", "promo=offer2", "promo=offer3", "promo=offer4", "promo=offer5", "promo=offer6", pytest.param("promo=offer7", marks=pytest.mark.xfail), "promo=offer8", "promo=offer9"])
+def test_guest_can_go_to_login_page(browser, test_site):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?{test_site}"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                         # открываем страницу
     page.ad_product_to_busket()
